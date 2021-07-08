3e43e3765070bd2d5c64d079247a3469e14b291a
Curtis Martin <curtbusiness@gmail.com>
2021-07-07 23:56:40 -0500

    Redux Actions

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { addComment } from '../redux/ActionCreators';
 
 import Home from './HomeComponent';
 import Directory from './DirectoryComponents';
@@ -18,6 +19,10 @@ const mapStateToProps = state => {
         partners: state.partners,
         promotions: state.promotions
     };
+}
+
+const mapDispatchToProps = {
+    addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text))
 };
 
 class Main extends Component {
@@ -36,14 +41,11 @@ class Main extends Component {
 
         const CampsiteWithId = ({match}) => {
             return (
-                <div>
                 <CampsiteInfo 
                     campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
                     comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                    addComment={this.props.addComment}
                 />
-                </div>
-                
-
             );
         }; 
 
@@ -66,4 +68,4 @@ class Main extends Component {
 
 
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
